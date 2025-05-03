78cde4bbbb5c1524a87a51a10ec7cf3a974d161e
AuanJulio <auanjulio13@gmail.com>
2025-05-03 00:14:44 -0300

    first commit

---
+++ src/main/java/com/priceCrawler/PriceCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.priceCrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceCrawlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/priceCrawler/PriceCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priceCrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
